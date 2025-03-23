ddbb9dab8114761a5eca67a597ffcf77456f30dd
Jucaski <thanididbefore@gmail.com>
2025-03-23 17:24:19 -0600

    change value depending on selected sickness

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -2,10 +2,13 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { MapComponent } from '../../components/map/map.component';
 import { DiseaseDbService } from '../../services/disease-db.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SicknessGroupList, SicknessKey } from '../../models/sickness-group-list';
 
 @Component({
   selector: 'app-main',
-  imports: [MapComponent],
+  imports: [MapComponent, CommonModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
   standalone: true
@@ -14,20 +17,24 @@ export class MainComponent implements OnInit {
   @ViewChild('mapContainer') mapContainer!: ElementRef;
 
   constructor(private dbService: DiseaseDbService) { }
-  data: any;
+  sicknessList: any[] = [];
+  selectedSickness: string = "Selecciona una opción";
+  groupList: any[] = [];
 
   ngOnInit() {
     this.dbService.getDisease('cve_enfermedad', 'enfermedad')
       .subscribe({
         next: (response) => {
-          this.data = response;
-          console.log("Se logro :)");
-          console.log(JSON.stringify(this.data));
+          this.sicknessList = response;
+          console.log(JSON.stringify(this.sicknessList));
         },
         error: (error) => {
           console.error('Error fetching data:', error);
         }
       });
+  }
+  onSicknessChange(sicknessNumber: SicknessKey) {
+    this.groupList = SicknessGroupList[sicknessNumber]
   }
   
   showAndScrollToMap() {
