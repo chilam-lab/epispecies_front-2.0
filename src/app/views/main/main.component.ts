abd73a7e107cba18d7dc7a641e46169b242633cc
Jucaski <thanididbefore@gmail.com>
2025-11-04 14:12:22 -0600

    design for month, station in the period modal

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -100,6 +100,22 @@ export class MainComponent implements OnInit {
   }
   showAgeTotals: [string,number][]= []
   showGenderTotals:[string,number][]= []
+  selectedSeason: string = '';
+  selectedMonth: number = 0;
+  monthsList = [
+    { value: 1, name: 'Enero' },
+    { value: 2, name: 'Febrero' },
+    { value: 3, name: 'Marzo' },
+    { value: 4, name: 'Abril' },
+    { value: 5, name: 'Mayo' },
+    { value: 6, name: 'Junio' },
+    { value: 7, name: 'Julio' },
+    { value: 8, name: 'Agosto' },
+    { value: 9, name: 'Septiembre' },
+    { value: 10, name: 'Octubre' },
+    { value: 11, name: 'Noviembre' },
+    { value: 12, name: 'Diciembre' }
+  ];
   notification = Swal.mixin({
     toast: true,
     position: "bottom-end",
@@ -585,4 +601,6 @@ export class MainComponent implements OnInit {
     this.periodCloseRef.nativeElement.click();
   }
 
+  selectedPeriod(season: string){
+  }
 }
