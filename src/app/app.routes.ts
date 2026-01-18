1299af2f74436246b3c04ee6fa4d1d40f277c8eb
Jucaski <thanididbefore@gmail.com>
2026-01-17 21:44:21 -0600

    add no-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './views/main/main.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
+    { path: '**', component: NotFoundComponent }
 ];
